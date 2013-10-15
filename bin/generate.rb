d63229bbfcbf3ecaad4bf1cd8e9db79d0e88ec35
irmiller22 <irmiller22@gmail.com>
2013-10-15 19:56:11 -0400

    Files for most recent update -- working on ERB templates

---
+++ bin/generate.rb
@@ -0,0 +1,16 @@
+require_relative '../config/environment.rb'
+
+class SiteGenerate
+
+	# index = ERB.new(File.open('../_site/index.erb').read)
+	# artists = Artist.all
+
+	# File.open('../_site/index.html', 'w+') do |f|
+	# 	f << index.result(binding)
+	# end
+
+# artist = ERB.new(File.open)
+
+
+
+end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative '../lib/models/artist.rb'
 require_relative '../lib/models/genre.rb'
 require_relative '../lib/models/song.rb'
 require_relative '../lib/models/parser.rb'
+require_relative '../bin/generate.rb'
+require_relative '../bin/cli.rb'
+
+
+require 'erb'
