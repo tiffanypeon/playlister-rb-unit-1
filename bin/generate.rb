393394dc4f5fd2aa0971d3a76e0e170a931ad069
David Bella <dbella1@gmail.com>
2013-10-16 11:46:06 -0400

    Added some erb templates, fixed a few more bugs along the way :)

--- bin/generate.rb
+++ bin/generate.rb
@@ -1,16 +1,38 @@
 require_relative '../config/environment.rb'
+require 'erb'
+require 'youtube_search'
 
 class SiteGenerate
 
-	# index = ERB.new(File.open('../_site/index.erb').read)
-	# artists = Artist.all
+  def generate
+    artist_index = ERB.new(File.open('lib/views/artist_index.erb').read)
 
-	# File.open('../_site/index.html', 'w+') do |f|
-	# 	f << index.result(binding)
-	# end
+    File.open('_site/artists.html', 'w+') do |f|
+      f << artist_index.result
+    end
 
-# artist = ERB.new(File.open)
+    genre_index = ERB.new(File.open('lib/views/genre_index.erb').read)
 
+    File.open('_site/genres.html', 'w+') do |f|
+      f << genre_index.result
+    end
+    
+    artist_profile = ERB.new(File.open('lib/views/artist_profile.erb').read)
 
+    Artist.all.each do |artist_object|
+      File.open("_site/artists/#{artist_object.name.gsub(' ', '_')}.html", 'w+') do |f|
+        f << artist_profile.result(binding)
+      end
+    end
+  end
 
 end
+
+p = Parser.new('')
+p.call
+
+@artists = Artist.all
+@genres = Genre.all
+
+g = SiteGenerate.new
+g.generate
